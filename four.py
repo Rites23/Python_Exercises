bfa051aa0c51b4e88c94cb11982a70109fb900a6
RITESH <ritesh.balu@gmail.com>
2025-12-09 15:52:40 +0200

    variables_exercise

---
+++ four.py
@@ -0,0 +1,11 @@
+# Create two variables:
+# - movie — a string storing the title of your favorite movie
+# - views — a number representing how many times you’ve watched it
+#
+# Your program should print:
+# I watched <movie> about <views> number of times
+
+movie = "The Shawshank Redemption"
+views = 12
+
+print(f"I watched {movie} about {views} number of times")
